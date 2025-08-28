4e793fd2966406684f00f0baff1da9ffab8a8454
Abul Hassan <imabulhasan99@gmail.com>
2025-08-28 20:59:37 +0600

    Add new resource methods to MailerLite facade

--- src/Facades/MailerLite.php
+++ src/Facades/MailerLite.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace Ihasan\LaravelMailerlite\Facades;
 
+use Ihasan\LaravelMailerlite\Resources\Automations\AutomationBuilder;
 use Ihasan\LaravelMailerlite\Resources\Campaigns\CampaignBuilder;
+use Ihasan\LaravelMailerlite\Resources\Fields\FieldBuilder;
 use Ihasan\LaravelMailerlite\Resources\Groups\GroupBuilder;
+use Ihasan\LaravelMailerlite\Resources\Segments\SegmentBuilder;
 use Ihasan\LaravelMailerlite\Resources\Subscribers\SubscriberBuilder;
+use Ihasan\LaravelMailerlite\Resources\Webhooks\WebhookBuilder;
 use Illuminate\Support\Facades\Facade;
 
 /**
@@ -18,6 +22,10 @@ use Illuminate\Support\Facades\Facade;
  * @method static SubscriberBuilder subscribers()
  * @method static CampaignBuilder campaigns()
  * @method static GroupBuilder groups()
+ * @method static FieldBuilder fields()
+ * @method static SegmentBuilder segments()
+ * @method static AutomationBuilder automations()
+ * @method static WebhookBuilder webhooks()
  *
  * @see \Ihasan\LaravelMailerlite\LaravelMailerlite
  */
