ce8499da86a207a6e34501c2033672d810d27322
Abul Hassan <imabulhasan99@gmail.com>
2025-08-28 21:01:00 +0600

    Add services and builders for all MailerLite resources

--- src/LaravelMailerliteServiceProvider.php
+++ src/LaravelMailerliteServiceProvider.php
@@ -4,11 +4,29 @@ declare(strict_types=1);
 
 namespace Ihasan\LaravelMailerlite;
 
+use Ihasan\LaravelMailerlite\Contracts\AutomationsInterface;
+use Ihasan\LaravelMailerlite\Contracts\CampaignsInterface;
+use Ihasan\LaravelMailerlite\Contracts\FieldsInterface;
+use Ihasan\LaravelMailerlite\Contracts\GroupsInterface;
 use Ihasan\LaravelMailerlite\Contracts\MailerLiteInterface;
+use Ihasan\LaravelMailerlite\Contracts\SegmentsInterface;
 use Ihasan\LaravelMailerlite\Contracts\SubscribersInterface;
+use Ihasan\LaravelMailerlite\Contracts\WebhooksInterface;
 use Ihasan\LaravelMailerlite\Manager\MailerLiteManager;
+use Ihasan\LaravelMailerlite\Resources\Automations\AutomationBuilder;
+use Ihasan\LaravelMailerlite\Resources\Automations\AutomationService;
+use Ihasan\LaravelMailerlite\Resources\Campaigns\CampaignBuilder;
+use Ihasan\LaravelMailerlite\Resources\Campaigns\CampaignService;
+use Ihasan\LaravelMailerlite\Resources\Fields\FieldBuilder;
+use Ihasan\LaravelMailerlite\Resources\Fields\FieldService;
+use Ihasan\LaravelMailerlite\Resources\Groups\GroupBuilder;
+use Ihasan\LaravelMailerlite\Resources\Groups\GroupService;
+use Ihasan\LaravelMailerlite\Resources\Segments\SegmentBuilder;
+use Ihasan\LaravelMailerlite\Resources\Segments\SegmentService;
 use Ihasan\LaravelMailerlite\Resources\Subscribers\SubscriberBuilder;
 use Ihasan\LaravelMailerlite\Resources\Subscribers\SubscriberService;
+use Ihasan\LaravelMailerlite\Resources\Webhooks\WebhookBuilder;
+use Ihasan\LaravelMailerlite\Resources\Webhooks\WebhookService;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -42,15 +60,69 @@ class LaravelMailerliteServiceProvider extends PackageServiceProvider
             return MailerLiteManager::fromConfig($config);
         });
 
-        // Register subscriber service
+        // Register services
         $this->app->bind(SubscribersInterface::class, SubscriberService::class);
         $this->app->bind(SubscriberService::class, function ($app) {
             return new SubscriberService($app->make(MailerLiteManager::class));
         });
 
-        // Register subscriber builder
+        $this->app->bind(CampaignsInterface::class, CampaignService::class);
+        $this->app->bind(CampaignService::class, function ($app) {
+            return new CampaignService($app->make(MailerLiteManager::class));
+        });
+
+        $this->app->bind(GroupsInterface::class, GroupService::class);
+        $this->app->bind(GroupService::class, function ($app) {
+            return new GroupService($app->make(MailerLiteManager::class));
+        });
+
+        $this->app->bind(FieldsInterface::class, FieldService::class);
+        $this->app->bind(FieldService::class, function ($app) {
+            return new FieldService($app->make(MailerLiteManager::class));
+        });
+
+        $this->app->bind(SegmentsInterface::class, SegmentService::class);
+        $this->app->bind(SegmentService::class, function ($app) {
+            return new SegmentService($app->make(MailerLiteManager::class));
+        });
+
+        $this->app->bind(AutomationsInterface::class, AutomationService::class);
+        $this->app->bind(AutomationService::class, function ($app) {
+            return new AutomationService($app->make(MailerLiteManager::class));
+        });
+
+        $this->app->bind(WebhooksInterface::class, WebhookService::class);
+        $this->app->bind(WebhookService::class, function ($app) {
+            return new WebhookService($app->make(MailerLiteManager::class));
+        });
+
+        // Register builders
         $this->app->bind(SubscriberBuilder::class, function ($app) {
             return new SubscriberBuilder($app->make(SubscriberService::class));
+        });
+
+        $this->app->bind(CampaignBuilder::class, function ($app) {
+            return new CampaignBuilder($app->make(CampaignService::class));
+        });
+
+        $this->app->bind(GroupBuilder::class, function ($app) {
+            return new GroupBuilder($app->make(GroupService::class));
+        });
+
+        $this->app->bind(FieldBuilder::class, function ($app) {
+            return new FieldBuilder($app->make(FieldService::class));
+        });
+
+        $this->app->bind(SegmentBuilder::class, function ($app) {
+            return new SegmentBuilder($app->make(SegmentService::class));
+        });
+
+        $this->app->bind(AutomationBuilder::class, function ($app) {
+            return new AutomationBuilder($app->make(AutomationService::class));
+        });
+
+        $this->app->bind(WebhookBuilder::class, function ($app) {
+            return new WebhookBuilder($app->make(WebhookService::class));
         });
 
         // Register main service
