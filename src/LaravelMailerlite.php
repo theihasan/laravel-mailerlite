e0432820b11b1f42213d33950cac02008eae1e80
Abul Hassan <imabulhasan99@gmail.com>
2025-08-28 21:00:02 +0600

    Add builder methods for MailerLite resources

--- src/LaravelMailerlite.php
+++ src/LaravelMailerlite.php
@@ -6,7 +6,13 @@ namespace Ihasan\LaravelMailerlite;
 
 use Ihasan\LaravelMailerlite\Contracts\MailerLiteInterface;
 use Ihasan\LaravelMailerlite\Manager\MailerLiteManager;
+use Ihasan\LaravelMailerlite\Resources\Automations\AutomationBuilder;
+use Ihasan\LaravelMailerlite\Resources\Campaigns\CampaignBuilder;
+use Ihasan\LaravelMailerlite\Resources\Fields\FieldBuilder;
+use Ihasan\LaravelMailerlite\Resources\Groups\GroupBuilder;
+use Ihasan\LaravelMailerlite\Resources\Segments\SegmentBuilder;
 use Ihasan\LaravelMailerlite\Resources\Subscribers\SubscriberBuilder;
+use Ihasan\LaravelMailerlite\Resources\Webhooks\WebhookBuilder;
 
 /**
  * Laravel MailerLite Main Service Class
@@ -29,5 +35,53 @@ class LaravelMailerlite implements MailerLiteInterface
     public function subscribers(): SubscriberBuilder
     {
         return app(SubscriberBuilder::class);
+    }
+
+    /**
+     * Get a campaign builder instance.
+     */
+    public function campaigns(): CampaignBuilder
+    {
+        return app(CampaignBuilder::class);
+    }
+
+    /**
+     * Get a group builder instance.
+     */
+    public function groups(): GroupBuilder
+    {
+        return app(GroupBuilder::class);
+    }
+
+    /**
+     * Get a field builder instance.
+     */
+    public function fields(): FieldBuilder
+    {
+        return app(FieldBuilder::class);
+    }
+
+    /**
+     * Get a segment builder instance.
+     */
+    public function segments(): SegmentBuilder
+    {
+        return app(SegmentBuilder::class);
+    }
+
+    /**
+     * Get an automation builder instance.
+     */
+    public function automations(): AutomationBuilder
+    {
+        return app(AutomationBuilder::class);
+    }
+
+    /**
+     * Get a webhook builder instance.
+     */
+    public function webhooks(): WebhookBuilder
+    {
+        return app(WebhookBuilder::class);
     }
 }
