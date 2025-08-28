28f0da705f90e1e5ccbe59416ce9a6a2ccf7659d
Abul Hassan <imabulhasan99@gmail.com>
2025-08-28 17:46:52 +0600

    chore: configure git subtree aliases and update MailerLite facade with fluent API docs

--- src/Facades/LaravelMailerlite.php
+++ src/Facades/LaravelMailerlite.php
@@ -1,13 +1,27 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Ihasan\LaravelMailerlite\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Ihasan\LaravelMailerlite\Resources\Subscribers\SubscriberBuilder;
+use Ihasan\LaravelMailerlite\Resources\Campaigns\CampaignBuilder;
+use Ihasan\LaravelMailerlite\Resources\Groups\GroupBuilder;
 
 /**
+ * MailerLite Facade
+ *
+ * Provides a fluent, plain-English API for interacting with MailerLite services.
+ * This facade enables method chaining for readable and expressive code.
+ *
+ * @method static SubscriberBuilder subscribers()
+ * @method static CampaignBuilder campaigns()
+ * @method static GroupBuilder groups()
+ *
  * @see \Ihasan\LaravelMailerlite\LaravelMailerlite
  */
-class LaravelMailerlite extends Facade
+class MailerLite extends Facade
 {
     protected static function getFacadeAccessor(): string
     {
